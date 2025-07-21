5b5031a5e9ef689fdf02dcc7860982d62ed93703
mikotest4 <fakealok756@gmail.com>
2025-07-22 01:10:30 +0530

    Update tordownload.py

--- bot/core/tordownload.py
+++ bot/core/tordownload.py
@@ -1,4 +1,4 @@
-from os import path as ospath
+from os import path as ospath, listdir
 from aiofiles import open as aiopen
 from aiofiles.os import path as aiopath, remove as aioremove, mkdir
 
@@ -15,17 +15,44 @@ class TorDownloader:
     @handle_logs
     async def download(self, torrent, name=None):
         if torrent.startswith("magnet:"):
+            # Get list of files before download
+            files_before = set(listdir(self.__downdir)) if ospath.exists(self.__downdir) else set()
+            
             torp = TorrentDownloader(torrent, self.__downdir)
             await torp.start_download()
-            # Get the actual downloaded file name from torrent info
-            actual_name = torp._torrent_info._info.name()
-            return ospath.join(self.__downdir, actual_name)
+            
+            # Get list of files after download
+            files_after = set(listdir(self.__downdir)) if ospath.exists(self.__downdir) else set()
+            
+            # Find the new file(s) that were downloaded
+            new_files = files_after - files_before
+            if new_files:
+                # Get the largest file (usually the video file)
+                largest_file = max(new_files, key=lambda f: ospath.getsize(ospath.join(self.__downdir, f)))
+                return ospath.join(self.__downdir, largest_file)
+            
+            # Fallback: return the name parameter if provided
+            return ospath.join(self.__downdir, name) if name else None
+            
         elif torfile := await self.get_torfile(torrent):
+            # Get list of files before download
+            files_before = set(listdir(self.__downdir)) if ospath.exists(self.__downdir) else set()
+            
             torp = TorrentDownloader(torfile, self.__downdir)
             await torp.start_download()
             await aioremove(torfile)
-            actual_name = torp._torrent_info._info.name()
-            return ospath.join(self.__downdir, actual_name)
+            
+            # Get list of files after download
+            files_after = set(listdir(self.__downdir)) if ospath.exists(self.__downdir) else set()
+            
+            # Find the new file(s) that were downloaded
+            new_files = files_after - files_before
+            if new_files:
+                # Get the largest file (usually the video file)
+                largest_file = max(new_files, key=lambda f: ospath.getsize(ospath.join(self.__downdir, f)))
+                return ospath.join(self.__downdir, largest_file)
+            
+            return None
 
     @handle_logs
     async def get_torfile(self, url):
