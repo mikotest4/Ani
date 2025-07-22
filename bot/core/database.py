80005e83ea4a23f65566b968ba9f94e4d2d16866
mikotest4 <fakealok756@gmail.com>
2025-07-22 10:58:42 +0530

    Update database.py

--- bot/core/database.py
+++ bot/core/database.py
@@ -1,158 +1,123 @@
-from asyncio import sleep as asleep, gather
-from urllib.parse import parse_qs, urlparse, unquote
-from pyrogram.filters import command, private, user
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.errors import FloodWait, MessageNotModified
+from motor.motor_asyncio import AsyncIOMotorClient
+from datetime import datetime, timedelta
+from bot import Var, LOGS
 
-from bot import bot, bot_loop, Var, ani_cache
-from bot.core.database import db
-from bot.core.func_utils import decode, is_fsubbed, get_fsubs, editMessage, sendMessage, new_task, convertTime, getfeed
-from bot.core.auto_animes import get_animes
-from bot.core.reporter import rep
+class MongoDB:
+    def __init__(self, uri, database_name):
+        self.__client = AsyncIOMotorClient(uri)
+        self.__db = self.__client[database_name]
+        self.__animes = self.__db.animes[Var.BOT_TOKEN.split(':')[0]]
+        self.__users = self.__db.users
 
-@bot.on_message(command('start') & private)
-@new_task
-async def start_msg(client, message):
-    uid = message.from_user.id
-    from_user = message.from_user
-    
-    # Auto-add user to database
-    await db.add_user(
-        user_id=uid, 
-        name=from_user.first_name, 
-        username=from_user.username
-    )
-    
-    txtargs = message.text.split()
-    temp = await sendMessage(message, "<b>ᴄᴏɴɴᴇᴄᴛɪɴɢ..</b>")
-    if not await is_fsubbed(uid):
-        txt, btns = await get_fsubs(uid, txtargs)
-        return await editMessage(temp, txt, InlineKeyboardMarkup(btns))
-    if len(txtargs) <= 1:
-        await temp.delete()
-        btns = []
-        for elem in Var.START_BUTTONS.split():
-            try:
-                bt, link = elem.split('|', maxsplit=1)
-            except:
-                continue
-            if len(btns) != 0 and len(btns[-1]) == 1:
-                btns[-1].insert(1, InlineKeyboardButton(bt, url=link))
-            else:
-                btns.append([InlineKeyboardButton(bt, url=link)])
-        smsg = Var.START_MSG.format(first_name=from_user.first_name,
-                                    last_name=from_user.first_name,
-                                    mention=from_user.mention, 
-                                    user_id=from_user.id)
-        if Var.START_PHOTO:
-            await message.reply_photo(
-                photo=Var.START_PHOTO, 
-                caption=smsg,
-                reply_markup=InlineKeyboardMarkup(btns) if len(btns) != 0 else None
+    # Anime related methods
+    async def getAnime(self, ani_id):
+        botset = await self.__animes.find_one({'_id': ani_id})
+        return botset or {}
+
+    async def saveAnime(self, ani_id, ep, qual, post_id=None):
+        quals = (await self.getAnime(ani_id)).get(ep, {qual: False for qual in Var.QUALS})
+        quals[qual] = True
+        await self.__animes.update_one({'_id': ani_id}, {'$set': {ep: quals}}, upsert=True)
+        if post_id:
+            await self.__animes.update_one({'_id': ani_id}, {'$set': {"msg_id": post_id}}, upsert=True)
+
+    async def reboot(self):
+        await self.__animes.drop()
+
+    # User management methods
+    async def add_user(self, user_id, name=None, username=None):
+        """Add a user to the database"""
+        try:
+            user_data = {
+                '_id': user_id,
+                'name': name or str(user_id),
+                'username': username,
+                'join_date': datetime.now(),
+                'last_active': datetime.now()
+            }
+            await self.__users.update_one({'_id': user_id}, {'$set': user_data}, upsert=True)
+            return True
+        except Exception as e:
+            LOGS.error(f"Error adding user {user_id}: {e}")
+            return False
+
+    async def remove_user(self, user_id):
+        """Remove a user from the database"""
+        try:
+            await self.__users.delete_one({'_id': user_id})
+            return True
+        except Exception as e:
+            LOGS.error(f"Error removing user {user_id}: {e}")
+            return False
+
+    async def get_all_users(self):
+        """Get all user IDs"""
+        try:
+            users = []
+            async for user in self.__users.find({}):
+                users.append(user['_id'])
+            return users
+        except Exception as e:
+            LOGS.error(f"Error getting all users: {e}")
+            return []
+
+    async def get_user_count(self):
+        """Get total user count"""
+        try:
+            return await self.__users.count_documents({})
+        except Exception as e:
+            LOGS.error(f"Error getting user count: {e}")
+            return 0
+
+    async def user_exists(self, user_id):
+        """Check if user exists in database"""
+        try:
+            user = await self.__users.find_one({'_id': user_id})
+            return user is not None
+        except Exception as e:
+            LOGS.error(f"Error checking user {user_id}: {e}")
+            return False
+
+    async def get_user_info(self, user_id):
+        """Get user information"""
+        try:
+            return await self.__users.find_one({'_id': user_id})
+        except Exception as e:
+            LOGS.error(f"Error getting user info {user_id}: {e}")
+            return None
+
+    async def update_user(self, user_id, update_data):
+        """Update user data"""
+        try:
+            await self.__users.update_one({'_id': user_id}, {'$set': update_data})
+            return True
+        except Exception as e:
+            LOGS.error(f"Error updating user {user_id}: {e}")
+            return False
+
+    async def update_user_activity(self, user_id):
+        """Update user's last activity"""
+        try:
+            await self.__users.update_one(
+                {'_id': user_id}, 
+                {'$set': {'last_active': datetime.now()}},
+                upsert=False
             )
-        else:
-            await sendMessage(message, smsg, InlineKeyboardMarkup(btns) if len(btns) != 0 else None)
-        return
-    try:
-        arg = (await decode(txtargs[1])).split('-')
-    except Exception as e:
-        await rep.report(f"User : {uid} | Error : {str(e)}", "error")
-        await editMessage(temp, "<b>ɪɴᴘᴜᴛ ʟɪɴᴋ ᴄᴏᴅᴇ ᴅᴇᴄᴏᴅᴇ ғᴀɪʟᴇᴅ !</b>")
-        return
-    if len(arg) == 2 and arg[0] == 'get':
-        try:
-            fid = int(int(arg[1]) / abs(int(Var.FILE_STORE)))
+            return True
         except Exception as e:
-            await rep.report(f"User : {uid} | Error : {str(e)}", "error")
-            await editMessage(temp, "<b>ɪɴᴘᴜᴛ ʟɪɴᴋ ᴄᴏᴅᴇ ɪs ɪɴᴠᴀʟɪᴅ !</b>")
-            return
+            LOGS.error(f"Error updating user activity {user_id}: {e}")
+            return False
+
+    async def get_active_users(self, days=30):
+        """Get users active in the last N days"""
         try:
-            msg = await client.get_messages(Var.FILE_STORE, message_ids=fid)
-            if msg.empty:
-                return await editMessage(temp, "<b>ғɪʟᴇ ɴᴏᴛ ғᴏᴜɴᴅ !</b>")
-            nmsg = await msg.copy(message.chat.id, reply_markup=None)
-            await temp.delete()
-            if Var.AUTO_DEL:
-                async def auto_del(msg, timer):
-                    await asleep(timer)
-                    await msg.delete()
-                await sendMessage(message, f'<b>ғɪʟᴇ ᴡɪʟʟ ʙᴇ ᴅᴇʟᴇᴛᴇᴅ ɪɴ {convertTime(Var.DEL_TIMER)},  ғᴏʀᴡᴏʀᴅ ᴛᴏ sᴀᴠᴇᴅ ᴍᴇssᴀɢᴇs ɴᴏᴡ ..</b>')
-                bot_loop.create_task(auto_del(nmsg, Var.DEL_TIMER))
+            cutoff_date = datetime.now() - timedelta(days=days)
+            users = []
+            async for user in self.__users.find({'last_active': {'$gte': cutoff_date}}):
+                users.append(user['_id'])
+            return users
         except Exception as e:
-            await rep.report(f"User : {uid} | Error : {str(e)}", "error")
-            await editMessage(temp, "<b>ғɪʟᴇ ɴᴏᴛ ғᴏᴜɴᴅ !</b>")
-    else:
-        await editMessage(temp, "<b>ɪɴᴘᴜᴛ ʟɪɴᴋ ɪs ɪɴᴠᴀʟɪᴅ ғᴏʀ ᴜsᴀɢᴇ !</b>")
+            LOGS.error(f"Error getting active users: {e}")
+            return []
 
-# Add user activity tracking to all message handlers
-@bot.on_message(private)
-@new_task
-async def track_user_activity(client, message):
-    """Track user activity for all private messages"""
-    if message.from_user:
-        await db.update_user_activity(message.from_user.id)
-
-@bot.on_message(command('pause') & private & user(Var.ADMINS))
-async def pause_fetch(client, message):
-    ani_cache['fetch_animes'] = False
-    await sendMessage(message, "<b>sᴜᴄᴄᴇssғᴜʟʟʏ ᴘᴀᴜsᴇᴅ ғᴇᴛᴄʜɪɴɢ ᴀɴɪᴍᴇ...</b>")
-
-@bot.on_message(command('resume') & private & user(Var.ADMINS))
-async def pause_fetch(client, message):
-    ani_cache['fetch_animes'] = True
-    await sendMessage(message, "<b>sᴜᴄᴄᴇssғᴜʟʟʏ ʀᴇsᴜᴍᴇᴅ ғᴇᴛᴄʜɪɴɢ ᴀɴɪᴍᴇ...</b>")
-
-@bot.on_message(command('log') & private & user(Var.ADMINS))
-@new_task
-async def _log(client, message):
-    await message.reply_document("log.txt", quote=True)
-
-@bot.on_message(command('addlink') & private & user(Var.ADMINS))
-@new_task
-async def add_task(client, message):
-    if len(args := message.text.split()) <= 1:
-        return await sendMessage(message, "<b>ɴᴏ ʟɪɴᴋ ғᴏᴜɴᴅ ᴛᴏ ᴀᴅᴅ</b>")
-    
-    Var.RSS_ITEMS.append(args[1])
-    req_msg = await sendMessage(message, f"<b>ɢʟᴏʙᴀʟ ʟɪɴᴋ ᴀᴅᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ!</b>\n\n <b>• ᴀʟʟ ʟɪɴᴋ(s) :</b> {', '.join(Var.RSS_ITEMS)[:-2]}")
-
-@bot.on_message(command('addtask') & private & user(Var.ADMINS))
-@new_task
-async def add_task(client, message):
-    if len(args := message.text.split()) <= 1:
-        return await sendMessage(message, "<b>ɴᴏ ᴛᴀsᴋ ғᴏᴜɴᴅ ᴛᴏ ᴀᴅᴅ</b>")
-    
-    index = int(args[2]) if len(args) > 2 and args[2].isdigit() else 0
-    if not (taskInfo := await getfeed(args[1], index)):
-        return await sendMessage(message, "<b>ɴᴏ ᴛᴀsᴋ ғᴏᴜɴᴅ ᴛᴏ ᴀᴅᴅ ғᴏʀ ᴛʜᴇ ᴘʀᴏᴠɪᴅᴇᴅ ʟɪɴᴋ</b>")
-    
-    ani_task = bot_loop.create_task(get_animes(taskInfo.title, taskInfo.link, True))
-    await sendMessage(message, f"<b>ᴛᴀsᴋ ᴀᴅᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ!</b>\n\n • <b>ᴛᴀsᴋ ɴᴀᴍᴇ : {taskInfo.title}</b>\n • <b>ᴛᴀsᴋ ʟɪɴᴋ :</b> {args[1]}")
-
-@bot.on_message(command('addmagnet') & private & user(Var.ADMINS))
-@new_task
-async def add_magnet_task(client, message):
-    if len(args := message.text.split(maxsplit=1)) <= 1:
-        return await sendMessage(message, "<b>ɴᴏ ᴍᴀɢɴᴇᴛ ʟɪɴᴋ ғᴏᴜɴᴅ ᴛᴏ ᴀᴅᴅ</b>")
-    
-    magnet_link = args[1]
-    
-    # Extract name from magnet link
-    try:
-        parsed = parse_qs(urlparse(magnet_link).query)
-        anime_name = unquote(parsed['dn'][0]) if 'dn' in parsed else "Unknown Anime"
-    except:
-        anime_name = "Unknown Anime"
-    
-    # Send confirmation message
-    confirmation_msg = f"""✅ <b>ᴍᴀɢɴᴇᴛ ᴛᴀsᴋ ᴀᴅᴅᴇᴅ !</b>
-
-🔸 <b>ɴᴀᴍᴇ: {anime_name}</b>
-
-🧲 <b>ᴍᴀɢɴᴇᴛ: {magnet_link[:50]}...</b>"""
-    
-    await sendMessage(message, confirmation_msg)
-    
-    # Start processing the anime
-    ani_task = bot_loop.create_task(get_animes(anime_name, magnet_link, True))
-    await sendMessage(message, f"<b>ᴘʀᴏᴄᴇssɪɴɢ sᴛᴀʀᴛᴇᴅ !</b>\n\n• <b>ᴛᴀsᴋ ɴᴀᴍᴇ :</b> {anime_name}")
+db = MongoDB(Var.MONGO_URI, "FZAutoAnimes")
